8b206738e8c6b927a187a9c6aa7ac6ed04ceaf06
hasemi <h171754@gmail.com>
2020-10-20 20:53:26 +0900

    미사용 클래스 제거

--- src/main/java/com/chat/realtime/web/controller/UserController.java
+++ src/main/java/com/chat/realtime/web/controller/UserController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
-    private final SimpUserRegistry userRegistry;
 
     @PostMapping("/login")
     public UserSaveResponseDto login(@RequestBody UserSaveRequestDto requestDto) {
